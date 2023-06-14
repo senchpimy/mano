59b0b3e514d6a0c40171b97bfd325c399c389d89
senchpimy <senchpimy@gmail.com>
2023-06-13 19:50:26 -0600

    start

---
+++ main.py
@@ -0,0 +1,4 @@
+import serial
+import time
+
+ard = serial.Serial('COM3', 9600)
---
+++ test1/test1.ino
@@ -0,0 +1,10 @@
+int led=1;
+int led2 =2;
+int option;
+void setup() {
+	Serial.begin(9600);
+}
+
+void loop() {
+
+}
