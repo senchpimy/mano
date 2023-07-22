9fe2f8fae40448605e5a0edffc28a6025e6fc6ed
senchi <senchpimy@gmail.com>
2023-07-21 22:20:53 -0600

    medidas dedos

--- camara.py
+++ camara.py
@@ -18,9 +18,10 @@ def distancia(
     return np.sqrt(x**2+y**2)*1000
 
 def regla_de_tres(referencia:float, mapear:float)->int:
-    if mapear>referencia:return 180
-    v = referencia*180
-    p = v/mapear
+    #if mapear>referencia:return 180
+    v = referencia/350
+    p = v*mapear
+    if int(p)>180:return 180
     return int(p) 
 
 while webcam.isOpened():
@@ -30,10 +31,18 @@ while webcam.isOpened():
     if result.multi_hand_landmarks:
         for hand in result.multi_hand_landmarks:
             mitad = distancia(hand.landmark[mp_hand.HandLandmark.WRIST],hand.landmark[mp_hand.HandLandmark.MIDDLE_FINGER_MCP])
-            d = distancia(hand.landmark[mp_hand.HandLandmark.MIDDLE_FINGER_TIP],hand.landmark[mp_hand.HandLandmark.MIDDLE_FINGER_MCP])
-            print(regla_de_tres(mitad,d))
+            medio = distancia(hand.landmark[mp_hand.HandLandmark.MIDDLE_FINGER_TIP],hand.landmark[mp_hand.HandLandmark.MIDDLE_FINGER_MCP])
+            pulgar = distancia(hand.landmark[mp_hand.HandLandmark.THUMB_TIP],hand.landmark[mp_hand.HandLandmark.THUMB_MCP])
+            indice = distancia(hand.landmark[mp_hand.HandLandmark.INDEX_FINGER_MCP],hand.landmark[mp_hand.HandLandmark.INDEX_FINGER_TIP])
+            ring = distancia(hand.landmark[mp_hand.HandLandmark.RING_FINGER_MCP],hand.landmark[mp_hand.HandLandmark.RING_FINGER_TIP])
+            pinky = distancia(hand.landmark[mp_hand.HandLandmark.PINKY_MCP],hand.landmark[mp_hand.HandLandmark.PINKY_TIP])
+            print(regla_de_tres(mitad,medio))
+            print(regla_de_tres(mitad,indice*1.12)) # Relacion 0.9:1 con el medio
+            print(regla_de_tres(mitad,ring*1.26)) # Relacion 0.8:1 con el medio
+            print(regla_de_tres(mitad,pinky*1.58)) # Relacion 0.6:1 con el medio
+            #print(regla_de_tres(mitad,pulgar*1.25)) # Relacion 0.8:1 con el medio
             print(mitad)
-            print(d)
+            print(medio)
             mp_drawing_utils.draw_landmarks(img,hand, mp_hand.HAND_CONNECTIONS)
 
     cv2.imshow("image",img)
